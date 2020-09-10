ab74f8bb6c04996b6395318e3d6464a93c6d268d
GlassEyeStudio <dawid@witulski.org>
2020-09-10 06:29:14 +0200

    Added: uuid generator module

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,25 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <span> {{ getNewUUID() }}</span>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "./components/HelloWorld.vue";
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   components: {
     HelloWorld
   }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  getNewUUID() {
+    return uuidv4();
+  }
+}
 </script>
 
 <style lang="scss">
