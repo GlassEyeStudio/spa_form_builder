b27ac2be402ab30b0ea27db8028b20a79bd450c5
GlassEyeStudio <dawid@witulski.org>
2020-09-10 07:07:43 +0200

    Added: Form element component

---
+++ src/components/FormBuilderElems/Form.vue
@@ -0,0 +1,27 @@
+<template>
+  <form>
+    <component
+      v-for="page in formElem.items"
+      :key="page.uuid"
+      v-bind:is="PageElem"
+      :pageElem="page"
+    />
+  </form>
+</template>
+
+<script>
+  import { Component, Vue } from "vue-property-decorator";
+  import { v4 as uuidv4 } from "uuid";
+  import PageElem from "@/components/FormBuilderElems/Page.vue";
+
+  @Component({
+    name: "Form",
+    components: { PageElem },
+    props: { formElem: {} }
+  })
+  export default class Form extends Vue {
+    getNewUUID = () => uuidv4();
+  }
+</script>
+
+<style scoped></style>
